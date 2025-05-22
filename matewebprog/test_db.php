fa5deef786c2913861048701e8b4ca54f115d3a0
MolnarMate <molnarmate050@gmail.com>
2025-05-22 10:09:26 +0200

    Delete matewebprog directory

--- matewebprog/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'mateadatb', 'M8i7S6i5', 'mateadatb');
-
-if ($mysqli->connect_error) {
-    die('Sikertelen kapcsolat: ' . $mysqli->connect_error);
-} else {
-    echo 'Sikeres kapcsolat!';
-}
